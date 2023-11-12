c575bf0ccaafad163d9b79c2b9e01a063127450c
Allan <allandrs@gmail.com>
2023-11-12 00:58:56 -0300

    teste

--- src/br/com/alura/loja/TestaImposto.java
+++ src/br/com/alura/loja/TestaImposto.java
@@ -5,14 +5,14 @@ import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
-import static br.com.alura.loja.imposto.TipoImposto.ISS;
+
 
 public class TestaImposto {
     public static void main(String[] args) {
 
         Orcamento orcamento = new Orcamento(new BigDecimal("100"));
         CalculadoraImposto calcular = new CalculadoraImposto();
-        System.out.println(calcular.calcular(orcamento, ISS));
+
 
 
 
